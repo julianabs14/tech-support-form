afa2023e4d5cc87eaef4ca555ba8e54916ac17aa
Juliana <juliianabs67@gmail.com>
2026-03-05 21:22:35 -0300

    first commit technical support form

---
+++ script.js
@@ -0,0 +1,6 @@
+function direcionarASucess() {
+    let btn = document.getElementById('submit');
+    btn.addEventListener("click", function() {
+        window.location.href = "nextpage.html";
+    });
+}
